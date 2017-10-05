d6da8f819a3bd7094484569d8faaef9f1d139517
EasternCatNZL <brianpwwong@gmail.com>
2017-10-06 12:53:41 +1300

    Started enemy movement

--- Hogei/Assets/Scripts/Enemy/Movement/EnemyPivotPointMovement.cs
+++ Hogei/Assets/Scripts/Enemy/Movement/EnemyPivotPointMovement.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyPivotPointMovement : MonoBehaviour {
 
+    //control vars
+    private Transform startPos;
+
+    //nav agent ref
+    private NavMeshAgent navAgent;
+
 	// Use this for initialization
 	void Start () {
-		
+        startPos = transform;
+        navAgent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    //get a random position around the starting pos
+
 }
