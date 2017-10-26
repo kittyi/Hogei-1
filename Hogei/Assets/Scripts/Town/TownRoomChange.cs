46a3fb6d0e27b1e93a65ab8d0acee0d59a24d40a
EasternCatNZL <brianpwwong@gmail.com>
2017-10-26 16:43:05 +1300

    Started on moving in towns

---
+++ Hogei/Assets/Scripts/Town/TownRoomChange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownRoomChange : MonoBehaviour {
+
+    [Header("Door refs")]
+    [Tooltip("Reference to door A")]
+    public TownDoorCheck doorA;
+    [Tooltip("Reference to door B")]
+    public TownDoorCheck doorB;
+
+    [Header("Tags")]
+    [Tooltip("Fader tag")]
+    public string fadeTag = "Fade";
+
+    //script refs
+    private FadeInFadeOut fader;
+
+    //control vars
+    private bool readyingMove = false;
+    private float timeFadeStart = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        fader = GameObject.FindGameObjectWithTag(fadeTag).GetComponent<FadeInFadeOut>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Move between the two rooms, based on which room detects player
+    private void BeginMove()
+    {
+        //check two doors, if either detects player, move player to other door
+        if (doorA.isPlayerPresent)
+        {
+            //fade in
+            fader.StartFadeIn();
+            //move player to other room after fade in done
+            
+        }
+    }
+
+    //move the player and fade out after a delay
+    private IEnumerator MoveRooms(TownDoorCheck a, TownDoorCheck b)
+    {
+        yield return new WaitForSeconds(fader.fadeSpeed);
+        
+    }
+}
---
+++ Hogei/Assets/Scripts/Utils/FadeInFadeOut.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInFadeOut : MonoBehaviour {
+
+    [Header("Timing vars")]
+    [Tooltip("Speed of one fade transition")]
+    public float fadeSpeed = 0.5f;
+
+    [Header("Image ref")]
+    public Image imageRef;
+
+    //control vars
+    private bool isFadeIn = false;
+    private bool isFadeOut = false;
+    private float alphaLevel = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FadeIn();
+        FadeOut();
+    }
+
+    //fade image alpha to black
+    private void FadeIn()
+    {
+        if (isFadeIn)
+        {
+            //change alpha relative to time
+            alphaLevel += fadeSpeed * Time.deltaTime;
+            //change image colour
+            imageRef.color = new Color(0, 0, 0, alphaLevel);
+            //keep values between 0 and 1
+            alphaLevel = Mathf.Clamp(alphaLevel, 0.0f, 1.0f);
+            //if done, stop
+            if (alphaLevel == 1)
+            {
+                isFadeIn = false;
+            }
+        }
+    }
+
+    //fade image alpha to transparent
+    private void FadeOut()
+    {
+        if (isFadeOut)
+        {
+            //change alpha relative to time
+            alphaLevel -= fadeSpeed * Time.deltaTime;
+            //change image colour
+            imageRef.color = new Color(0, 0, 0, alphaLevel);
+            //keep values between 0 and 1
+            alphaLevel = Mathf.Clamp(alphaLevel, 0.0f, 1.0f);
+            //if done, stop
+            if (alphaLevel == 0)
+            {
+                isFadeOut = false;
+            }
+        }
+    }
+
+    //funcs to start fading funcs
+    public void StartFadeIn()
+    {
+        isFadeIn = true;
+    }
+
+    public void StartFadeOut()
+    {
+        isFadeOut = true;
+    }
+}
