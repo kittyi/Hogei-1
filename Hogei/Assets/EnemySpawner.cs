6e5db1f1daab6198a5948332d90d6ed9dd1d9f94
Karl Graves <karlgraves28@gmail.com>
2017-11-10 13:15:09 +1300

    Spawner for tool

---
+++ Hogei/Assets/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public int Level = 4; //public for testing
+    public GameObject[] Enemy;
+    public Transform[] EnemySpawnPoints;
+    
+
+	// Use this for initialization
+	void Start () {
+        SpawnEnemies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void SpawnEnemies()
+    {
+        int EnemyIndex = Random.Range(0, Enemy.Length);
+   
+        for (int i = 0; i < Level; i++)
+        {
+            Instantiate(Enemy[Random.Range(0, Enemy.Length)], EnemySpawnPoints[i].position, Quaternion.identity);
+        }
+    }
+}
